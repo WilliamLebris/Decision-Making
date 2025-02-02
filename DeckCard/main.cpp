99375f1fcce37e8cfda26eb043ebc66a8ce2ea25
WilliamLebris <williamtissi2006@gmail.com>
2025-02-01 19:26:26 -0800

    organizing my code

--- DeckCard/main.cpp
+++ DeckCard/main.cpp
@@ -18,14 +18,20 @@ int main() {
         // Randomly select suit and value
         string suit = suits[rand() % 4];
         string value = values[rand() % 13];
-  
+    
+    // now tha we randomly chose the value, let calculate the length of the random card that you be picked
+    int sizeOfCard;
+    
+    sizeOfCard = suit.length() + value.length();
+    // now that we have the size, and we know for sure that everytime we run the program we will have 24 space taken by Casino cards:------- and we will have 2 space taken by "of" so we just have to calculate after how many of space we should put the last last to make it align.
+    int space = 40 -(sizeOfCard + 2 +24);
     // Now it is time to work on the output
     
     cout << setfill('*')<< setw(40)<< ""<<endl;  // print a line of 40 stars at the top
     cout << setfill('*')<<setw(1)<<""<<setfill(' ')<<setw(38)<<""<<setfill('*')<<setw(1)<<""<<endl;
     
     // print the card description including '-'
-    cout << setfill ('*') << setw(1) << "" << setfill(' ') << setw (2) << "" << "Casino cards:" << setfill ('-') << setw (7) <<"" << value<<" of "<<suit << setfill(' ') << setw (2) << "" << setfill('*') << setw (1) << "" << endl;
+    cout << setfill ('*') << setw(1) << "" << setfill(' ') << setw (2) << "" << "Casino cards:" << setfill ('-') << setw (6) <<"" << value<<" of "<<suit << setfill(' ') << setw (space-1) << "" << setfill('*') << setw (1) << "" << endl;
   
     cout << setfill('*')<<setw(1)<<""<<setfill(' ')<<setw(38)<<""<<setfill('*')<<setw(1)<<""<<endl;
     cout << setfill('*')<< setw(40)<< ""<<endl<< endl;
@@ -36,9 +42,8 @@ int main() {
 /*
  ****************************************
  *                                      *
- *  Casino cards:-------Eight of Clubs  *
+ *  Casino cards:------Five of Clubs    *
  *                                      *
  ****************************************
-
  Program ended with exit code: 0
  */
--- Manipulating-console-output/main.cpp
+++ Manipulating-console-output/main.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  Manipulating-console-output
@@ -6,43 +7,88 @@
 //
 #include <iostream>
 #include <iomanip> // For manipulators like setw, setfill, fixed, setprecision
+#include <string>  // For to_string()
 
 using namespace std;
 
 int main() {
     // Initialization of my int variables
-    int id1 = 1, id2 = 2, id3 = 3;
+    int id1 = 1, id2 = 2, id3 = 3;  //I used 1 instead of 00001 because as an integer, even if i type 0001 it will just print 1
     
-    //Initialization of my string variables
+    // Initialization of my string variables
     string name1 = "Alice", name2 = "Bob", name3 = "Charlie";
     
-//    Initialization of my float variables
+    // Initialization of my float variables
     float balance1 = 1000.00, balance2 = 530.25, balance3 = 2040.50;
     
-    //now let's print the output. since the average max character is around 24, I center the daily report after 5 character
+    // Now let's print the output. Since the average max character is around 24, I center the daily report after 5 characters
     
-    cout <<setfill(' ')<<setw(5)<<""<< "Daily report"<< endl;
-    cout << "ID"<<setfill(' ')<< setw(6)<<""<<"Name"<<setfill(' ')<<setw(4)<<""<<"Balance"<<endl;
+    cout << setfill(' ') << setw(5) << "" << "Daily report" << endl;
     
-    //printing the line that separate the description wih the values
-    cout <<setfill('-')<<setw(6)<<""<<setfill(' ')<<setw(2)<<""<<setfill('-')<<setw(6)<<""<<setfill(' ')<<setw(2)<<""<<setfill('-')<<setw(6)<<""<<endl;
+    // for the program to keep be nice, the number shouldn't be more than 8, the name shouldn't be more than 10, at most 11 and the balance can be long as it want since it is the last variable
+    cout << "ID" << setfill(' ') << setw(6) << "" << "Name" << setfill(' ') << setw(7) << "" << "Balance" << endl;
     
-    //now printing the variables in a specific format 
-    cout<<setfill('0')<<setw(4)<<""<<id1<<setfill(' ')<<setw(3)<<""<<name1<<setfill(' ')<<setw(3)<<""<<fixed<<setprecision(2)<<"$"<<balance1<<endl;
+    // Printing the line that separates the description with the values
+    cout << setfill('-') << setw(7) << "" << setfill(' ') << setw(1) << "" << setfill('-') << setw(10) << "" << setfill(' ') << setw(1) << "" << setfill('-') << setw(9) << "" << endl;
     
-    cout<<setfill('0')<<setw(4)<<""<<id2<<setfill(' ')<<setw(3)<<""<<name2<<setfill(' ')<<setw(5)<<""<<right<<fixed<<setprecision(2)<<"$"<<balance2<<endl;
+    // Now printing the variables in a specific format with calculated lengths
     
-    cout<<setfill('0')<<setw(4)<<""<<id3<<setfill(' ')<<setw(3)<<""<<name3<<setfill(' ')<<setw(1)<<""<<fixed<<setprecision(2)<<"$"<<balance3<<endl;
+    // Calculate the length of ID, name, and balance
+    string idLength1 = to_string(id1);
+    string nameLength1 = name1;
+    string balanceLength1 = to_string(balance1);
+
+    // Ensure name starts after 8 characters, we have four zero before the number so i included the 4 in the calculation
     
+    int spaceFromIdToName = 8 - (idLength1.length() + 4);
     
+    // Ensure balance starts after 19 characters  I calculated the number of character taken so far and subtract 19
+    int spaceFromNameToBalance = 19 - (spaceFromIdToName  + (nameLength1.length() ) + idLength1.length() + 4);
+    
+    // Printing row for id1, name1, balance1
+    cout << setfill('0') << setw(4) << "" << id1
+         << setfill(' ') << setw(spaceFromIdToName) << ""
+         << name1
+         << setfill(' ') << setw(spaceFromNameToBalance) << ""
+         << fixed << setprecision(2) << "$" << balance1 << endl;
+
+    // Repeat for id2, name2, balance2
+    string idLength2 = to_string(id2);
+    string nameLength2 = name2;
+    string balanceLength2 = to_string(balance2);
+
+    int spaceFromIdToName2 = 8 - (idLength2.length() + 4);
+    int spaceFromNameToBalance2 = 19 - (spaceFromIdToName2 + nameLength2.length() + idLength2.length() +4);
+
+    cout << setfill('0') << setw(4) << "" << id2
+         << setfill(' ') << setw(spaceFromIdToName2) << ""
+         << name2
+         << setfill(' ') << setw(spaceFromNameToBalance2) << ""
+         << fixed << setprecision(2) << "$" << balance2 << endl;
+
+    // Repeat for id3, name3, balance3
+    string idLength3 = to_string(id3);
+    string nameLength3 = name3;
+    string balanceLength3 = to_string(balance3);
+
+    int spaceFromIdToName3 = 8 - (idLength3.length() +4);
+    int spaceFromNameToBalance3 = 19 - (spaceFromIdToName3 + nameLength3.length() + idLength3.length() +4);
+
+    cout << setfill('0') << setw(4) << "" << id3
+         << setfill(' ') << setw(spaceFromIdToName3) << ""
+         << name3
+         << setfill(' ') << setw(spaceFromNameToBalance3) << ""
+         << fixed << setprecision(2) << "$" << balance3 << endl;
+
     return 0;
 }
+
 /*
  Daily report
-ID      Name    Balance
-------  ------  ------
-00001   Alice   $1000.00
-00002   Bob     $530.25
-00003   Charlie $2040.50
+ID      Name       Balance
+------- ---------- ---------
+00001   Alice      $1000.00
+00002   Bob        $530.25
+00003   Charlie    $2040.50
 Program ended with exit code: 0
  */
